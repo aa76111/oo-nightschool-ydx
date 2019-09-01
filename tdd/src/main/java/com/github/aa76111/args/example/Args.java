b539593953e7dcad84f379fce8d1e6f0bef90665
ydx <761113093@qq.com>
2019-09-01 12:37:17 +0800

    tdd:01-fizzbuzz #comment add method parse schema and args #time 0.5h

--- tdd/src/main/java/com/github/aa76111/args/example/Args.java
+++ tdd/src/main/java/com/github/aa76111/args/example/Args.java
@@ -1,7 +1,34 @@
 package com.github.aa76111.args.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Args {
+
+    private HashMap<String, ArgumentMarshaler> marshalers;
+    private HashSet<Character> argsFound;
+
     public Args(String schema, String[] args) throws ArgsException {
+        marshalers = new HashMap<>();
+        argsFound = new HashSet<>();
+        parseSchema(schema);
+        parseArgs(Arrays.asList(args));
+        
+    }
+
+    private void parseSchema(String schema) {
+        Arrays.stream(schema.split(","))
+                .filter(element -> element.length() > 0 )
+                .forEach(element -> parseSchemaElement(element.trim()));
+    }
+
+    private void parseSchemaElement(String element) {
+
+    }
+
+    private void parseArgs(List<String> args) {
 
     }
 }
